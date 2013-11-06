fc821220a5520d087ab50079d813a1c668f03851
darin <dbaumgartel@gmail.com>
2013-11-06 22:22:25 +0100

    updates for pdf error sets and histogram amalgamation

--- BlackHatHistogramMaker/blackhat_hists/HistoGather.py
+++ BlackHatHistogramMaker/blackhat_hists/HistoGather.py
@@ -1,35 +1,35 @@
 import os
 import sys
 
-# files = ['hists/'+x.replace('\n','') for x in os.popen('ls -1 hists').readlines()]
+files = ['hists/'+x.replace('\n','') for x in os.popen('ls -1 hists').readlines()]
 
-# filetypes = []
+filetypes = []
 
-# for f in files:
-# 	f = f.replace('real','*')
-# 	f = f.replace('loop','*')
-# 	f = f.replace('born','*')
-# 	f = f.replace('vsub','*')
+for f in files:
+	f = f.replace('real','*')
+	f = f.replace('loop','*')
+	f = f.replace('born','*')
+	f = f.replace('vsub','*')
 
-# 	for x in range(len(f)-6):
-# 		if f[x:x+4] == 'list':
-# 			numtag =  f[x+4:x+7]
-# 			# print numtag
-# 			f = f.replace(numtag,'*')
-# 	if f not in filetypes:
-# 		filetypes.append(f)
-# 	# print f
-# for f in filetypes:
-# 	outfile = f.replace('*.list*','_')
-# 	outfile = outfile.replace('.root','_all.root')
-# 	for x in range(10):
-# 		outfile = outfile.replace('__','_')
-# 	print './combineHistograms.exe -outfile '+outfile+' '+f
-# 	os.system('./combineHistograms.exe -outfile '+outfile+' '+f)
+	for x in range(len(f)-6):
+		if f[x:x+4] == 'list':
+			numtag =  f[x+4:x+7]
+			# print numtag
+			f = f.replace(numtag,'*')
+	if f not in filetypes:
+		filetypes.append(f)
+	# print f
+for f in filetypes:
+	outfile = f.replace('*.list*','_')
+	outfile = outfile.replace('.root','_all.root')
+	for x in range(10):
+		outfile = outfile.replace('__','_')
+	print './combineHistograms.exe -outfile '+outfile+' '+f
+	os.system('./combineHistograms.exe -outfile '+outfile+' '+f)
 
-# os.system('rm -r MergedHistos')
-# os.system('mkdir MergedHistos')
-# os.system('mv hists/*all.root MergedHistos/')
+os.system('rm -r MergedHistos')
+os.system('mkdir MergedHistos')
+os.system('mv hists/*all.root MergedHistos/')
 
 
 mfiles = files = ['MergedHistos/'+x.replace('\n','') for x in os.popen('ls -1 MergedHistos').readlines()]
